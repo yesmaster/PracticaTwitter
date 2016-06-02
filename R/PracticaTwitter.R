39d4bdb1bb81f67f9ccaa3f4d563df215c97525a
yesmaster <yesmastermail@gmail.com>
2016-06-02 21:37:54 +0200

    - Dp de classe

--- R/PracticaTwitter.R
+++ R/PracticaTwitter.R
@@ -20,13 +20,38 @@ twitteR::setup_twitter_oauth(consumer_key,
                              access_token,
                              access_token_secret)
 
+# User information extraction
+tuser <- twitteR::getUser("yesmastertweet") 
 
-# Tweet
-twitteR::tweet("bip...bip...")
+# Lists of friends/followers
+fr_names <- tuser$getFriends() # List of friends names
+fr_ids <- tuser$getFriendIDs() # List of friends IDs
+fol_names <- tuser$getFollowers() # List of followers names
+fol_ids <- tuser$getFollowerIDs()  # List of followers IDs
+usr_info <- tuser$toDataFrame("notwhatsoever", optional=FALSE) # Information from 1 user
 
-# Are they following me?
-x <- twitteR::friendships(screen_names = "notwhatsoever")
+# Friends information extraction
+fr_num <- tuser$getriendsCount()
+fr_ids <- tuser$getFriendIDs() # chr[] with friends IDs
+friends <- friendships(user_ids=fr_ids) # data.frame with friends information
+tfriends <- lookupUsers(friends$name) # friends information extraction 
 
-# Info about users with permission
-tuser <- twitteR::getUser("yesmastertweet")
-tusers <- twitteR::lookupUsers(c("yesmastertweet", "notwhatsoever"))
+# Followers information extraction
+fol_num <- tuser$getFollowersCount()
+fol_ids <- tuser$getFollowerIDs() # chr[] with followers IDs
+followers <- friendships(user_ids=fol_ids) # data.frame with followers information
+tfollowers <- lookupUsers(followers$name) # followers information extraction
+
+
+ttweets <- userTimeline(tuser, n=20)  # Last tweets
+ttweets[[1]]$getCreated() # Tweet date
+ttweets[[1]]$getId()  # Tweet id
+twitteR::tweet("bip...bip...")  # Tweet
+
+##############################################################
+
+# Search friends & friends of friends -> create a tree
+# Look for a user, its friends, their tweets, search a key word & extract results
+# Location??
+
+
