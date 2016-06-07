721f0a67b21c71786731d051baa2b1c12ed27139
yesmaster <yesmastermail@gmail.com>
2016-06-07 19:25:51 +0200

    Separar en funciones

---
+++ R/functions.R
@@ -0,0 +1,56 @@
+# FUNCTION: Load libraries to use
+loadLibraries <- function(){
+  if (!require("twitteR")) {
+    install.packages("twitteR")
+    library("twitteR")
+  }
+  if (!require("igraph")){
+    install.packages("igraph")
+    library("igraph")
+  }
+  if (!require("yaml")){
+    install.packages("yaml")
+    library("yaml")
+  } 
+}
+
+# FUNCTION: User authentication for Tweeter
+tweeterAuthentication <- function(){
+  auth = yaml :: yaml.load_file("data/auth.yml") # Load authentication file
+  
+  consumer_key <- auth$twitter_auth$consumer_key
+  consumer_secret <- auth$twitter_auth$consumer_secret
+  access_token <- auth$twitter_auth$access_token
+  access_token_secret <- auth$twitter_auth$access_token_secret
+  
+  options(httr_oauth_cache=T) #This will enable the use of a local file to cache OAuth access credentials between R sessions.
+  setup_twitter_oauth(consumer_key,
+                      consumer_secret,
+                      access_token,
+                      access_token_secret)
+}
+
+# FUNCTION: Users structure creation
+fillMatrixOfUsers <- function(x) {# User information extraction
+  tuser <- twitteR::getUser(x) 
+  friends_num <- tuser$getFriendsCount() # num of friends
+  friends_ids <- tuser$getFriendIDs() # List of friends IDs
+  friends <- friendships(user_ids=friends_ids) # data.frame with friends information
+  
+  # option 1 to generate tfriends
+  
+  tfriends <- lookupUsers(friends$name) # tuser structures with friends information 
+  # option 2 to generate tfriends
+  for(i in 1:friends_num)  {
+    tfriends[i] <<- twitteR::getUser(friends[[2]][[i]])
+  }
+} 
+
+# FUNCTION: Vector of bad language
+KeyWordsVector <- c("atontado", "baboso", "besugo", "bobo", "burro", "capullo",
+                    "cazurro", "ceporro", "cenutrio", "cipote", "cretino",
+                    "cutre", "chorra", "estúpido", "ganso", "gilipollas",
+                    "idiota", "lerdo", "malparido", "marica", "maricon",
+                    "memo", "mentecato", "pendejo", "percebe", "puto", "puta",
+                    "tarado", "tarugo", "tolondron", "tontarrón", "tonto", "torpe",
+                    "zángano")
